086079a4eeb554d1aa6281a11b6bb43e4db72765
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-09-24 12:06:53 -0500

    Add models

---
+++ models/Entry.ts
@@ -0,0 +1,31 @@
+import mongoose, { Model } from "mongoose"
+import { Entry } from "../interfaces"
+
+export interface IEntry extends Entry {}
+
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+  description: { type: String, required: true },
+  createdAt: { type: Number },
+  status: {
+    type: String,
+    enum: {
+      values: ["pending", "in-progress", "finished"],
+      message: "{VALUE} no es un estado válido}",
+    },
+  },
+})
+
+schema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const EntryModel: Model<IEntry> =
+  mongoose.models.Entry || mongoose.model("Entry", schema)
+
+export default EntryModel
